99175586a79402541f57ba6e1b4cb092268f575c
Franpan94 <francescopanico94@libero.it>
2022-09-22 15:59:52 +0200

    first commit

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Faker\Generator as Faker;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        //
+
+         $users = User::all();
+         
+        for($i = 0; $i < 50; $i++){
+            $newpost = new Post();
+            $newpost->user_id = $faker->randomElement($users)->id;
+            $newpost->title = $faker->sentence(3);
+            $newpost->post_content = $faker->paragraph(20);
+            $newpost->post_image = $faker->imageUrl();
+            $newpost->post_date = $faker->dateTime();
+            $newpost->save();
+        }
+    }
+}
+
